c04340dbda716ea69d75a7eb10d3aae5663974ed
Milaxx07 <camilabomfim2007@gmail.com>
2025-09-29 20:10:20 -0300

    Add files via upload
    Questão 4

---
+++ MainPilha.java
@@ -0,0 +1,21 @@
+public class MainPilha{
+    
+    public static void main(String[] args) {
+        Pilha minhaPilha = new Pilha(5);
+        System.out.println("Pilha com capacidade 5 criada.");
+
+        minhaPilha.push(10);
+        minhaPilha.push(20);
+        minhaPilha.push(30);
+        System.out.println("Valores 10, 20 e 30 inseridos.");
+
+        System.out.println("Elemento no topo: " + minhaPilha.peek());
+
+        System.out.println("Removendo elementos da pilha...");
+        while (!minhaPilha.isEmpty()) {
+            System.out.println("Removido: " + minhaPilha.pop());
+        }
+
+        System.out.println("A pilha está vazia? " + minhaPilha.isEmpty());
+    }
+}
